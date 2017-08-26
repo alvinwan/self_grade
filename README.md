f3e62c8593bbaca73e8aa32de1809f1781fe485a
Alvin Wan <hi@alvinwan.com>
2017-08-26 14:44:23 -0700

    added form generation

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# self-grade
+# Self Grading Infrastructure
+
 self-grading HTML generator and Gradescope autograder
+
+* Generates self-grading form from LaTeX
+* Self-grading form automatically generates and downloads JSON file
+
+# Installation
+
+Install Python packages via `pip`.
+
+```
+pip -r requirements.txt
+```
+
+# HTML Usage
+
+To generate a self-grade form, use the `html/create_form.py` script.
+
+```
+cd html
+python create_form.py path/to/tex
+```
+
+The script by default looks for `\Question{...}` commands, and for each question, looks for `\Part` commands. The outputted file is saved to the current directory. 
+
+# Gradescope Autograder
---
+++ html/create_form.py
@@ -0,0 +1,36 @@
+import jinja2
+import sys
+import os
+
+arguments = sys.argv
+filename = arguments[1]
+
+with open(filename) as f:
+    tex = f.read()
+
+questions = []
+question_ids = ['name', 'email', 'sid', 'duration_homework', 'duration_grade']
+context = {'questions': questions}
+
+questions_tex = tex.split('\\Question')
+for id_, question in enumerate(questions_tex[1:]):
+    num_parts = question.count('\Part')
+    for part in range(num_parts):
+        questions.append({
+            'title': 'Question %d Part %d' % (id_, part),
+            'id': "%d_%d" % (id_, part)
+        })
+        question_ids.append("%d_%d" % (id_, part))
+    if num_parts == 0:
+        questions.append({
+            'title': 'Question %d' % id_,
+            'id': str(id_)
+        })
+        question_ids.append(str(id_))
+
+context['question_ids'] = '"%s"' % '","'.join(question_ids)
+env = jinja2.Environment(loader=jinja2.FileSystemLoader('.'))
+out_path = os.path.basename(filename.replace('.tex', '.html'))
+with open(out_path, 'w') as f:
+    f.write(env.get_template('template.html').render(context))
+
